46b14d8cce205fae1db6d0952d53f986c87bbba9
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-06 10:05:12 +0200

    main program, read cli args and pass to file processor

---
+++ KenzeChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace KenzeChallenge;
+class Program   
+{
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<InputArgs>(args)
+            .WithParsed(options =>
+            {
+                if (!string.IsNullOrWhiteSpace(options.InputFile))
+                {
+                    var result = FileProcessor.ProcessFile(options.InputFile, options.TargetLength);
+
+                    // for testing purposes to see if it all works, could be removed when logging output to a file
+                    foreach (var line in result)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Unable to read input file argument.");
+                }
+            });
+
+    }
+}
