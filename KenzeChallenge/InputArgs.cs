e99f1974693f13da3f44e444bf8151cf3997d862
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-06 10:04:44 +0200

    create inputArgs class for cli args

---
+++ KenzeChallenge/InputArgs.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace KenzeChallenge;
+
+public class InputArgs
+{
+    [Option('i', "input", Required = true, HelpText = "Input file to be processed.")]
+    public string InputFile { get; set; } = null!;
+
+    [Option('t', "target", Required = true, HelpText = "Word length target.")]
+    public int TargetLength { get; set; }
+}
